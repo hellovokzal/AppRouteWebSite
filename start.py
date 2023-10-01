e3c0c1a74f5b1ef1d8f42159e03114a091433b85
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-10-01 20:43:18 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def hello():
-	return "Привет Настя, го трахаться?)) <3))"
+	return render_template("file.html")
 	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080)
