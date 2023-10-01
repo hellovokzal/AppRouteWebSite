0f4fc9afa04af9aaf750c0d0c08536b4b4ae4ee2
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-10-01 21:11:41 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -6,7 +6,28 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-	return render_template("file.html")
+	return """
+
+ <!DOCTYPE html>
+<html>
+  <head>
+    <title>Трахать</title>
+  </head>
+  <body>
+    <h1>Трахать</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG6i7Cb1Fa6cHYGrdHHQHyyVvclDfQu9wKgwAhJajRW6hmECA&s" alt="Изображение">
+    <br><br>
+    <button onclick="showSuccessMessage()">Трахать</button>
+    <p id="successMessage"></p>
+
+    <script>
+      function showSuccessMessage() {
+        document.getElementById("successMessage").innerHTML = "Успешно Трахан!";
+      }
+    </script>
+  </body>
+</html>
+"""
 	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080)
