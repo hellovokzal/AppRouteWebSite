909efba9e36c95fe825ada7e677220eefec484bd
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-10-01 19:57:04 +0500

    Update start.py

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-	return "Привет))"
+	return "Привет Настя, го трахаться?)) <3))"
 	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080)
